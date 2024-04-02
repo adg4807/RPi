bd8accce96b111b12f9def51f328a7ff07aeda8b
lee_JA <81452897+adg4807@users.noreply.github.com>
2024-04-02 19:09:11 +0900

    Update HW2-1.cpp

--- HW2-1.cpp
+++ HW2-1.cpp
@@ -1,82 +1,82 @@
 #include <iostream>
 #include <wiringPi.h>
 #include <unistd.h>
-
 using namespace std;
 
-#define LED_PIN_1 17
-#define LED_PIN_2 18
-#define LED_PIN_3 27
-#define BUTTON_PIN_1 13
-#define BUTTON_PIN_2 19
-#define BUTTON_PIN_3 26
-#define DEBOUNCE_TIME 300 // 디바운싱을 위한 대기 시간 (밀리초)
+#define RLED_GPIO      17
+#define RBUTTON_GPIO   13
+#define GLED_GPIO      27
+#define GBUTTON_GPIO   19 
+#define YLED_GPIO      18
+#define YBUTTON_GPIO   26 
 
-bool ledState_1 = false;
-bool ledState_2 = false;
-bool ledState_3 = false;
+#define DEBOUNCE_TIME 300
+bool stateRLED =false;
+bool stateGLED =false;
+bool stateYLED =false;
 
-unsigned long lastButtonPressTime_1 = 0;
-unsigned long lastButtonPressTime_2 = 0;
-unsigned long lastButtonPressTime_3 = 0;
 
-void toggleLED_1() {
-    unsigned long currentTime = millis();
-    if (currentTime - lastButtonPressTime_1 >= DEBOUNCE_TIME) {
-        ledState_1 = !ledState_1;
-        digitalWrite(LED_PIN_1, ledState_1 ? HIGH : LOW);
-        cout << "Button 1 pressed! LED 1 toggled." << endl;
-        lastButtonPressTime_1 = currentTime;
-    }
+void lightRLED(void){
+  static unsigned long lastISRTime = 0,  num = 1;
+  unsigned long currentISRTime = millis();
+  if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
+    digitalWrite(RLED_GPIO, !stateRLED);     
+    stateRLED = !stateRLED;
+    if(stateRLED)
+      cout << "Red Button pressed " << num++ << " times! RedLED on" << endl;
+    else
+      cout << "Red Button pressed " << num++ << " times! RedLED off" << endl;
+  }
+  lastISRTime = currentISRTime;
 }
 
-void toggleLED_2() {
-    unsigned long currentTime = millis();
-    if (currentTime - lastButtonPressTime_2 >= DEBOUNCE_TIME) {
-        ledState_2 = !ledState_2;
-        digitalWrite(LED_PIN_2, ledState_2 ? HIGH : LOW);
-        cout << "Button 2 pressed! LED 2 toggled." << endl;
-        lastButtonPressTime_2 = currentTime;
-    }
+void lightGLED(void){
+  static unsigned long lastISRTime = 0,  num = 1;
+  unsigned long currentISRTime = millis();
+  if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
+    digitalWrite(GLED_GPIO, !stateGLED);     
+    stateGLED = !stateGLED;
+    if(stateGLED)
+      cout << "Green Button pressed " << num++ << " times! GreenLED on" << endl;
+    else
+      cout << "Green Button pressed " << num++ << " times! GreenLED off" << endl;
+  }
+  lastISRTime = currentISRTime;
 }
 
-void toggleLED_3() {
-    unsigned long currentTime = millis();
-    if (currentTime - lastButtonPressTime_3 >= DEBOUNCE_TIME) {
-        ledState_3 = !ledState_3;
-        digitalWrite(LED_PIN_3, ledState_3 ? HIGH : LOW);
-        cout << "Button 3 pressed! LED 3 toggled." << endl;
-        lastButtonPressTime_3 = currentTime;
-    }
+void lightYLED(void){
+  static unsigned long lastISRTime = 0,  num = 1;
+  unsigned long currentISRTime = millis();
+  if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
+    digitalWrite(YLED_GPIO, !stateYLED);     
+    stateYLED = !stateYLED;
+    if(stateYLED)
+      cout << "Yellow Button pressed " << num++ << " times! YellowLED on" << endl;
+    else
+      cout << "Yellow Button pressed " << num++ << " times! YellowLED off" << endl;
+  }
+  lastISRTime = currentISRTime;
 }
 
-int main() {
-    wiringPiSetupGpio();
-
-    pinMode(LED_PIN_1, OUTPUT);
-    pinMode(LED_PIN_2, OUTPUT);
-    pinMode(LED_PIN_3, OUTPUT);
-    pinMode(BUTTON_PIN_1, INPUT);
-    pinMode(BUTTON_PIN_2, INPUT);
-    pinMode(BUTTON_PIN_3, INPUT);
-
-    digitalWrite(LED_PIN_1, LOW);
-    digitalWrite(LED_PIN_2, LOW);
-    digitalWrite(LED_PIN_3, LOW);
-
-    cout << "Press buttons on GPIO " << BUTTON_PIN_1 << ", " << BUTTON_PIN_2 << ", " << BUTTON_PIN_3 << endl;
-
-    // Attach interrupts to buttons
-    wiringPiISR(BUTTON_PIN_1, INT_EDGE_FALLING, &toggleLED_1);
-    wiringPiISR(BUTTON_PIN_2, INT_EDGE_FALLING, &toggleLED_2);
-    wiringPiISR(BUTTON_PIN_3, INT_EDGE_FALLING, &toggleLED_3);
-
-    // Main loop
-    while (true) {
-        // Your main code goes here
-        // This loop will keep your program running
-        delay(1000); // Delay for stability
-    }
-
-    return 0;
+int main() {                             
+  wiringPiSetupGpio();                  
+  pinMode(RLED_GPIO, OUTPUT);  
+  pinMode(GLED_GPIO, OUTPUT);
+  pinMode(YLED_GPIO, OUTPUT);
+  pinMode(RBUTTON_GPIO, INPUT);
+  pinMode(GBUTTON_GPIO, INPUT);
+  pinMode(YBUTTON_GPIO, INPUT);
+  digitalWrite (RLED_GPIO, LOW);
+  digitalWrite (GLED_GPIO, LOW);
+  digitalWrite (YLED_GPIO, LOW);
+  
+  wiringPiISR(RBUTTON_GPIO, INT_EDGE_RISING, &lightRLED);
+  wiringPiISR(GBUTTON_GPIO, INT_EDGE_RISING, &lightGLED);
+  wiringPiISR(YBUTTON_GPIO, INT_EDGE_RISING, &lightYLED);
+  for(; ; ){ 
+    int i=0;
+    cout << "You have " << i << " seconds remaining..." << endl;
+    sleep(1);                         
+  }
+  return 0;                             
 }
