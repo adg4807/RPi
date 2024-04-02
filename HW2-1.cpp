0b92100f41f211dd7059c53a151c0680f0d4e85d
lee_JA <81452897+adg4807@users.noreply.github.com>
2024-04-02 19:50:57 +0900

    Update HW2-1.cpp

--- HW2-1.cpp
+++ HW2-1.cpp
@@ -3,80 +3,72 @@
 #include <unistd.h>
 using namespace std;
 
-#define RLED_GPIO      17
-#define RBUTTON_GPIO   13
-#define GLED_GPIO      27
-#define GBUTTON_GPIO   19 
-#define YLED_GPIO      18
-#define YBUTTON_GPIO   26 
+#define RLED_GPIO 17
+#define GLED_GPIO 18
+#define YLED_GPIO 27
 
-#define DEBOUNCE_TIME 300
-bool stateRLED =false;
-bool stateGLED =false;
-bool stateYLED =false;
+#define RBUTTON_GPIO 13
+#define GBUTTON_GPIO 19
+#define YBUTTON_GPIO 26
 
+#define DEBOUNCE_TIME 200
 
-void lightRLED(void){
-  static unsigned long lastISRTime = 0,  num = 1;
-  unsigned long currentISRTime = millis();
-  if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
-    digitalWrite(RLED_GPIO, !stateRLED);     
-    stateRLED = !stateRLED;
-    if(stateRLED)
-      cout << "Red Button pressed " << num++ << " times! RedLED on" << endl;
-    else
-      cout << "Red Button pressed " << num++ << " times! RedLED off" << endl;
-  }
-  lastISRTime = currentISRTime;
+volatile bool rLED_state = false;
+volatile bool gLED_state = false;
+volatile bool yLED_state = false;
+
+static unsigned long lastISRTime1 = 0;
+static unsigned long lastISRTime2 = 0;
+static unsigned long lastISRTime3 = 0;
+
+void toggleLEDRed(void) {
+    unsigned long currentISRTime1 = millis();
+    if (currentISRTime1 - lastISRTime1 > DEBOUNCE_TIME) {
+        rLED_state = !rLED_state;
+        digitalWrite(RLED_GPIO, rLED_state ? HIGH : LOW);
+    }
+    lastISRTime1 = currentISRTime1;
 }
 
-void lightGLED(void){
-  static unsigned long lastISRTime = 0,  num = 1;
-  unsigned long currentISRTime = millis();
-  if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
-    digitalWrite(GLED_GPIO, !stateGLED);     
-    stateGLED = !stateGLED;
-    if(stateGLED)
-      cout << "Green Button pressed " << num++ << " times! GreenLED on" << endl;
-    else
-      cout << "Green Button pressed " << num++ << " times! GreenLED off" << endl;
-  }
-  lastISRTime = currentISRTime;
+void toggleLEDGreen(void) {
+    unsigned long currentISRTime2 = millis();
+    if (currentISRTime2 - lastISRTime2 > DEBOUNCE_TIME) {
+        gLED_state = !gLED_state;
+        digitalWrite(GLED_GPIO, gLED_state ? HIGH : LOW);
+    }
+    lastISRTime2 = currentISRTime2;
 }
 
-void lightYLED(void){
-  static unsigned long lastISRTime = 0,  num = 1;
-  unsigned long currentISRTime = millis();
-  if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
-    digitalWrite(YLED_GPIO, !stateYLED);     
-    stateYLED = !stateYLED;
-    if(stateYLED)
-      cout << "Yellow Button pressed " << num++ << " times! YellowLED on" << endl;
-    else
-      cout << "Yellow Button pressed " << num++ << " times! YellowLED off" << endl;
-  }
-  lastISRTime = currentISRTime;
+void toggleLEDYellow(void) {
+    unsigned long currentISRTime3 = millis();
+    if (currentISRTime3 - lastISRTime3 > DEBOUNCE_TIME) {
+        yLED_state = !yLED_state;
+        digitalWrite(YLED_GPIO, yLED_state ? HIGH : LOW);
+    }
+    lastISRTime3 = currentISRTime3;
 }
 
-int main() {                             
-  wiringPiSetupGpio();                  
-  pinMode(RLED_GPIO, OUTPUT);  
-  pinMode(GLED_GPIO, OUTPUT);
-  pinMode(YLED_GPIO, OUTPUT);
-  pinMode(RBUTTON_GPIO, INPUT);
-  pinMode(GBUTTON_GPIO, INPUT);
-  pinMode(YBUTTON_GPIO, INPUT);
-  digitalWrite (RLED_GPIO, LOW);
-  digitalWrite (GLED_GPIO, LOW);
-  digitalWrite (YLED_GPIO, LOW);
-  
-  wiringPiISR(RBUTTON_GPIO, INT_EDGE_RISING, &lightRLED);
-  wiringPiISR(GBUTTON_GPIO, INT_EDGE_RISING, &lightGLED);
-  wiringPiISR(YBUTTON_GPIO, INT_EDGE_RISING, &lightYLED);
-  for(; ; ){ 
-    int i=0;
-    cout << "You have " << i << " seconds remaining..." << endl;
-    sleep(1);                         
-  }
-  return 0;                             
+int main() {
+    wiringPiSetupGpio();
+    pinMode(RLED_GPIO, OUTPUT);
+    pinMode(GLED_GPIO, OUTPUT);
+    pinMode(YLED_GPIO, OUTPUT);
+
+    pinMode(RBUTTON_GPIO, INPUT);
+    pinMode(GBUTTON_GPIO, INPUT);
+    pinMode(YBUTTON_GPIO, INPUT);
+
+    digitalWrite(RLED_GPIO, LOW);
+    digitalWrite(GLED_GPIO, LOW);
+    digitalWrite(YLED_GPIO, LOW);
+
+    wiringPiISR(RBUTTON_GPIO, INT_EDGE_FALLING, &toggleLEDRed);
+    wiringPiISR(GBUTTON_GPIO, INT_EDGE_FALLING, &toggleLEDGreen);
+    wiringPiISR(YBUTTON_GPIO, INT_EDGE_FALLING, &toggleLEDYellow);
+
+    while (1) {
+        delay(1000);
+    }
+    return 0;
 }
+
